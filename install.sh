77d46f060d31d6723bb410de0ed58180f436a96c
raghuprathap <raghuprathap478@gmail.com>
2018-08-23 15:28:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 set -e
 cd $(dirname $0)
 
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
 sudo apt-get install ansible
 ansible-galaxy install -r roles.yml -p roles
 ansible-playbook playbook.yml
